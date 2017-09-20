26814e5605b9fb8933a168531bde8ab72b8022a3
DIVYA BHARATHI RAJARAJAN <divyaraja1602@gmail.com>
2017-09-20 20:39:02 +0530

    Create Solution.java

---
+++ Java/Strings/Java Regex/Solution.java
@@ -0,0 +1,31 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+class MyRegex extends Solution
+{
+    String IP = this.IP;
+    String pattern = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
+ boolean matches(){
+Pattern  p = Pattern.compile(pattern);
+    Matcher m = p.matcher(IP);
+    boolean ans = m.matches();
+    return ans;
+}
+    
+}
