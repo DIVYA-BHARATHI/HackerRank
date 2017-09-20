aa807b5f42d65e2a897040fa588c33f21f3e23ac
DIVYA BHARATHI RAJARAJAN <divyaraja1602@gmail.com>
2017-09-20 14:24:44 +0530

    Create Solution.java

---
+++ 30 Days Of Code/Data Types/Solution.java
@@ -0,0 +1,14 @@
+      import java.util.*;
+      import java.io.*;
+      
+      public class Solution
+      {
+        Scanner scan = new Scanner(System.in);
+        int j = scan.nextInt();
+        double e = scan.nextDouble();
+        String t = scan.nextLine();
+        String u = scan.nextLine();
+        System.out.println(i+j);
+        System.out.println(d + e );
+        System.out.println(s + u);
+          }
